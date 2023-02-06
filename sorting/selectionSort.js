9440b539f8c8dc65a7fec06d7af50614ed4fa9e5
HensalDeon <120702682+HensalDeon@users.noreply.github.com>
2023-02-06 18:45:07 +0530

    Update selectionSort.js

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -12,3 +12,21 @@ function selectionSort(array) {
     return array
 }
 console.log(selectionSort(array));
+
+//recursive selection sort
+function recursiveSelectionSort(array, i) {
+    if (i > array.length - 1) {
+        return array;
+    }
+
+    let j = i + 1;
+    while (j < array.length) {
+        if (array[i] > array[j]) {
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        j++;
+    }
+
+    return recursiveSelectionSort(array, i + 1);
+}
+console.log(recursiveSelectionSort(array, (i = 0)));
