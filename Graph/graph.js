b4b7aaf3827b329fe1f5feabeaf0043e3729fe02
hensal <hensaldeon777@gmail.com>
2023-11-22 13:32:14 +0530

    graph- shortest path

--- Graph/graph.js
+++ Graph/graph.js
@@ -41,6 +41,7 @@ class Graph {
     }
 
     breadthFirstSearch(startVertex) {
+        if (!this.adjacencyList[startVertex]) return null;
         const visited = new Set();
         const queue = [startVertex];
         let result = [];
@@ -61,6 +62,7 @@ class Graph {
     }
 
     depthFirstSearch(startVertex) {
+        if (!this.adjacencyList[startVertex]) return null;
         const visited = new Set();
         let result = [];
 
@@ -79,6 +81,31 @@ class Graph {
         return result;
     }
 
+    shortestPath(startVertex, endVertex) {
+        const visited = new Set();
+        const queue = [startVertex];
+        visited.add(startVertex);
+
+        while (queue.length > 0) {
+            const path = queue.shift();
+            const currentVertex = path[path.length - 1];
+
+            if (currentVertex === endVertex) {
+                return path;
+            }
+
+            for (let adjacentVertex of this.adjacencyList[currentVertex]) {
+                if (!visited.has(adjacentVertex)) {
+                    visited.add(adjacentVertex);
+                    const newPath = [...path, adjacentVertex];
+                    queue.push(newPath);
+                }
+            }
+        }
+
+        return null;
+    }
+
     display() {
         for (let vertex in this.adjacencyList) {
             console.log(vertex + "->" + [...this.adjacencyList[vertex]]);
@@ -87,21 +114,22 @@ class Graph {
 }
 
 const graph = new Graph();
-graph.addVertex("A")
-graph.addVertex("B")
-graph.addVertex("C")
-graph.addVertex("D")
-graph.addVertex("E")
-graph.addVertex("F")
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+// graph.addVertex("F")
 
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
 
-graph.addEdge("A", "B")
-graph.addEdge("A", "C")
-graph.addEdge("B","D")
-graph.addEdge("C","E")
-graph.addEdge("D","E")
-graph.addEdge("D","F")
-graph.addEdge("E","F")
+console.log("Shortest path", graph.shortestPath("A", "D"));
 
 graph.display();
 console.log(graph.hasEdge("A", "C"));
@@ -109,8 +137,8 @@ console.log(graph.hasEdge("A", "C"));
 // graph.removeEdge("A", "B");
 // graph.display();
 
-// graph.removeVertex("A");
-// graph.display()
+graph.removeVertex("A");
+graph.display();
 
-console.log(graph.breadthFirstSearch("A"));
-console.log(graph.depthFirstSearch("A"));
+// console.log(graph.breadthFirstSearch("A"));
+// console.log(graph.depthFirstSearch("A"));
